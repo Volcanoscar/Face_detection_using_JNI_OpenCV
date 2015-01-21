d056dc5c93d3b2b874cb2f1f4c854e844818c89f
allonq <y.qian42@gmail.com>
2015-01-20 20:04:36 -0800

    Seed project for further JNI work
    A Seed project for future JNI work, after all, the config of JNI is so
    verbose
    This small app uses OpenCV’s own machine learning and detection
    function.

---
+++ face-detection/obj/local/armeabi-v7a/objs-debug/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs-debug/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/assert.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/math.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/assert.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/math.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/qianyujing/NVPACK/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/log.h:
